dcaf498da376c50e0e66b1355b0b87bf72d9bbbc
Tohminn <GabeVOwens@gmail.com>
2018-06-24 22:53:48 -0500

    Testing JSON response

--- api/public/index.php
+++ api/public/index.php
@@ -1,12 +1,26 @@
 <?php
     header("Access-Control-Allow-Origin: *");
+    header('Content-Type: application/json');
+
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo 'Hello World';
     }    
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['username']) && isset($_POST['password'])) {
-            echo '{"login": true, "username": "'.$_POST['username'].'"';
+            $response = [
+                'login' => True,
+                'username' => $_POST['username'],
+                'password' => $_POST['password'],
+            ];
+            echo json_encode($response);
+        }else{
+            $response = [
+                'login' => False,
+                'username' => $_POST['username'],
+                'password' => $_POST['password'],
+            ];
+            echo json_encode($response);
         }
     }
 
