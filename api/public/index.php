be5894543ac7ff1693e4f522561bec6be23ec86f
Tohminn <GabeVOwens@gmail.com>
2018-06-24 23:13:57 -0500

    Changed from array to object

--- api/public/index.php
+++ api/public/index.php
@@ -9,11 +9,11 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $requestBody = file_get_contents('php://input');
         $post_vars = json_decode($requestBody);
-        if (isset($post_vars['username']) && isset($post_vars['password'])) {
+        if (isset($post_vars->username) && isset($post_vars->password)) {
             $response = [
                 'login' => True,
-                'username' => $post_vars['username'],
-                'password' => $post_vars['password'],
+                'username' => $post_vars->username,
+                'password' => $post_vars->password,
             ];
             echo json_encode($response);
         }else{
