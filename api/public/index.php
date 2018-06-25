ae35409d9389076b682dd8294a00e2fa262f4734
Tohminn <GabeVOwens@gmail.com>
2018-06-24 22:46:48 -0500

    Testing API & CORS setup

--- api/public/index.php
+++ api/public/index.php
@@ -6,7 +6,7 @@
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['username']) && isset($_POST['password'])) {
-            echo 'login api';
+            echo '{"login": true, "username": "'.$_POST['username'].'"';
         }
     }
 
